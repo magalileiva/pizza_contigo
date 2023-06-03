b32e2ac28e9f5fea8db10c84eff12c54a581458f
magali <magaliali13@gmail.com>
2023-06-04 00:53:08 +0200

    Menu completo creacion espacio ajustes responsive

--- franquicia.js
+++ franquicia.js
@@ -10,18 +10,16 @@ function processForm(e) {
     const email = e.currentTarget[1].value;
     const telefono = e.currentTarget[2].value;
     const mensaje = e.currentTarget[3].value;
+    const capital = e.currentTarget[4].value; 
     const mensajeFranquicia = new Object();
     mensajeFranquicia.nombre = nombre;
     mensajeFranquicia.email = email;
     mensajeFranquicia.telefono = telefono;
     mensajeFranquicia.mensaje = mensaje;
+    mensajeFranquicia.capital = capital;
     mensajesFranquicia.push(mensajeFranquicia);
 
     localStorage.setItem("test", JSON.stringify(mensajesFranquicia));
-
-    for(var i=0; i<mensajesFranquicia.length; i++){
-        console.log(mensajesFranquicia[i].nombre);
-    }
 }
 
 var form = document.getElementById('contact-us');
